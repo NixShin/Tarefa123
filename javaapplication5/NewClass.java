d54584e5e4b6907507d1fd898ad1cf0ee2559819
NixShin <124297263+NixShin@users.noreply.github.com>
2023-03-16 16:30:38 -0300

    Add files via upload

---
+++ javaapplication5/NewClass.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+    public class NewClass
+    {
+        public static void main(String[] args)
+        {
+        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+        radioButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        radioButtonFrame.setSize(300,100);
+        radioButtonFrame.setVisible(true);
+        
+        }
+    }
